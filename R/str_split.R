d329c9afd379200016d96b31c4b998b62503b579
sodium-hydroxide <43483259+sodium-hydroxide@users.noreply.github.com>
2023-11-26 19:06:05 -0700

    Added description and license

--- R/str_split.R
+++ R/str_split.R
@@ -1,8 +1,21 @@
-#' Split a string into comp
+#' Split a string into string vector
 #'
-#' This is where the documentation of my function begins.
-#' ...
+#' @description
+#' `str_split` is an extension of `base::strsplit` but returns a vector instead
+#' of a list. This function also removes blank characters and spaces.
+#'
+#' @param in_str Input string to be split
+#' @param splt Character to split at. By default, split at spaces.
 #' @export
+#'
+#' @examples
+#' str_split("Hello World")
+#' ## c("Hello", "World")
+#'
+#' str_split("Hello World")
+#' ## c("H", "e", "l", "l", "o", "W", "o", "r", "l", "d")
+
+#------------------------------------------------------------------------------#
 str_split <- function(in_str, splt = " ") {
   in_str <- strsplit(in_str, split = splt)[[1]]
 
