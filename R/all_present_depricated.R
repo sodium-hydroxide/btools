d17b371998983c6ac7c9df39bc3b1abe1fd9b341
sodium-hydroxide <43483259+sodium-hydroxide@users.noreply.github.com>
2024-03-24 05:49:07 -0600

    Allin creation

--- R/all_present_depricated.R
+++ R/all_present_depricated.R
@@ -1,13 +1,15 @@
 #' Check if all values are present in set
 #'
+#' Function is now deprecated. Please use %allin%
+#'
 #' @param ... Values to check
 #' @param set Vector containing values
 #'
-#' @return TRUE or FALSE if all values are present in set
+#' @return NULL
 #' @export
 #'
 all_present <- function(
-        ...,
-        set = c()) {
-    return(FALSE %in% (c(...) %in% set))
+    ...,
+    set = c()) {
+  stop("")
 }
